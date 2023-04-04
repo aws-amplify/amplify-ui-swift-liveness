439716c15b0ee866e5ba8c06b65ce443c919bef6
Ian Saultz <52051793+atierian@users.noreply.github.com>
2023-04-04 17:08:36 -0400

    chore: add internal helper utilities

---
+++ Sources/FaceLiveness/Utilities/Color+DynamicColors.swift
@@ -0,0 +1,24 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import UIKit
+import SwiftUI
+
+extension Color {
+    static func dynamicColors(light: UIColor, dark: UIColor) -> Color {
+        Color(
+            UIColor(
+                dynamicProvider: { traitCollection in
+                    switch traitCollection.userInterfaceStyle {
+                    case .dark: return dark
+                    default: return light
+                    }
+                }
+            )
+        )
+    }
+}
---
+++ Sources/FaceLiveness/Utilities/Color+Liveness.swift
@@ -0,0 +1,50 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import SwiftUI
+
+extension Color {
+    static let livenessPrimaryBackground = Color.dynamicColors(
+        light: .hex("#047D95"),
+        dark: .hex("#7DD6E8")
+    )
+
+    static let livenessPrimaryLabel = Color.dynamicColors(
+        light: .white,
+        dark: .hex("#0D1926")
+    )
+
+    static let livenessBackground = Color.dynamicColors(
+        light: .white,
+        dark: .hex("#0D1926")
+    )
+
+    static let livenessLabel = Color.dynamicColors(
+        light: .black,
+        dark: .white
+    )
+
+    static let livenessErrorBackground = Color.dynamicColors(
+        light: .hex("#950404"),
+        dark: .hex("#EF8F8F")
+    )
+
+    static let livenessErrorLabel = Color.dynamicColors(
+        light: .white,
+        dark: .black
+    )
+
+    static let livenessWarningBackground = Color.dynamicColors(
+        light: .hex("#F5D9BC"),
+        dark: .hex("#663300")
+    )
+
+    static let livenessWarningLabel = Color.dynamicColors(
+        light: .hex("#663300"),
+        dark: .hex("#EFBF8F")
+    )
+}
