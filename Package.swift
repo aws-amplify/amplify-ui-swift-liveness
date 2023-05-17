21e6aec6efb3d2c067d149a37caca58eccc058ba
Ian Saultz <52051793+atierian@users.noreply.github.com>
2023-05-17 11:23:58 -0400

    update amplify version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["FaceLiveness"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/aws-amplify/amplify-swift", branch: "fix/liveness-errors")
+        .package(url: "https://github.com/aws-amplify/amplify-swift", from: "2.11.0")
     ],
     targets: [
         .target(
