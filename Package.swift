f64e040ce111490b0098dc6c3304adb35993707a
Ian Saultz <52051793+atierian@users.noreply.github.com>
2023-07-13 16:06:21 -0400

    add new close method in test mock

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["FaceLiveness"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/aws-amplify/amplify-swift", branch: "fix/expose-session-closure-liveness")
+        .package(url: "https://github.com/aws-amplify/amplify-swift", branch: "main") //2.14.1
     ],
     targets: [
         .target(
