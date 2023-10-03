88b5437de7853b5b09a5fd0a6ed10b708fcabac1
Tuan Pham <phantum@amazon.com>
2023-10-03 12:51:55 -0500

    chore: fix code comment typo

--- Sources/FaceLiveness/Utilities/String+Localizable.swift
+++ Sources/FaceLiveness/Utilities/String+Localizable.swift
@@ -10,7 +10,7 @@ import Foundation
 extension String {
     /// Looks for a localized value using this value as the key.
     /// If no localization is found in the current app's bundle,
-    /// it defaults to the one provided by Authenticator
+    /// it defaults to the one provided by Liveness
     func localized(comment: String = "") -> String {
         let defaultValue = NSLocalizedString(self, bundle: .module, comment: "")
         return NSLocalizedString(
