e6645d1196b16eaf60679f63ea30c19fe3d81854
Abhash Kumar Singh <thisisabhash@gmail.com>
2024-05-06 11:23:33 -0700

    chore: update amplify dependency

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["FaceLiveness"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/aws-amplify/amplify-swift", exact: "2.29.3")
+        .package(url: "https://github.com/aws-amplify/amplify-swift", exact: "2.33.3")
     ],
     targets: [
         .target(
