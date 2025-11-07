97aa9f1df9359e4022c647001c8c666e94d991a5
Abhash Kumar Singh <thisisabhash@gmail.com>
2025-11-07 13:44:05 -0800

    fix: capture weak self reference in dispatchqueue execute blocks

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["FaceLiveness"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/aws-amplify/amplify-swift", from: "2.51.2")
+        .package(url: "https://github.com/aws-amplify/amplify-swift", branch: "fix/liveness-memory-leaks")
     ],
     targets: [
         .target(
