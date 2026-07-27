d2fbb2cc81708e1320f0305467cfb7b3ff390e33
aws-amplify-ops <aws-amplify-ops@amazon.com>
2026-07-27 23:02:07 +0000

    chore: release 1.4.5 [skip ci]

--- Sources/FaceLiveness/Utilities/UserAgent.swift
+++ Sources/FaceLiveness/Utilities/UserAgent.swift
@@ -10,7 +10,7 @@ import InternalAmplifyCredentials
 
 struct UserAgentValues {
     
-    static let libVersion = "1.4.4"
+    static let libVersion = "1.4.5"
     static let libName = "amplify-ui-swift-face-liveness"
     
     let amplifyVersion: String
